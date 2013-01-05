b03b445e80b0ad5277c13dcf9c1d7154d0035113
7rans <transfire@gmail.com>
2013-01-05 13:23:30 -0500

    Start fucking autoload project.

---
+++ README.rb
@@ -0,0 +1,32 @@
+[Homepage](http://rubyworks.github.com/autoload) /
+[Report Issue](http://github.com/rubyworks/autoload/issues) /
+[Source Code](http://github.com/rubyworks/autoload)
+( [] )
+
+
+# [Autoload](#description)
+
+Offical word has it [autoload is dead](http://www.ruby-forum.com/topic/3036681).
+
+But like James Brown, "Autoload is alive!"
+
+
+## [Usage](#usage)
+
+```ruby
+  autoload 'Splinter', 'splinter'
+
+  Splinter
+```
+
+## [Limitations](#limitations)
+
+Autoload is not thread safe. You need thread safe autoload? Submit a patch
+please.
+
+
+## [Copyrights](#copyrights)
+
+Hot Pancakes License
+
+Use at your discretion, just don't feed the lawyers.
---
+++ lib/autoload.rb
@@ -0,0 +1,44 @@
+$AUTOLOAD = Hash.new{ |h, k| h[k] = [] }
+
+#
+#
+#
+def self.autoload(name, path)
+  $AUTOLOAD[name.to_sym] << path
+end
+
+class Module
+  #
+  #
+  #
+  def self.autoload(subname, path)
+    $AUTOLOAD["#{name}::#{subname}".to_sym] << path
+  end
+end
+
+module Kernel
+  #
+  # Note: I am not so certain the instance level method is a good idea.
+  # The end used can just as easily and more cleary use `self.class.autoload`
+  # to do it themselves.
+  #
+  def autoload(subname, path)
+    $AUTOLOAD["#{self.class.name}::#{subname}".to_sym] << path
+  end
+end
+
+class Object
+  #
+  #
+  #
+  def self.const_missing(name)
+    if paths = $AUTOLOAD.delete(name)
+      paths.each do |path|
+        require(path)
+      end
+    else
+      super(name)
+    end
+  end
+end
+
