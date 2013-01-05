5ccb2c5aaf49438e33622107c7a63b4e3fefb68e
7rans <transfire@gmail.com>
2013-01-05 13:36:08 -0500

    First commit.

---
+++ HISTORY.rb
@@ -0,0 +1,11 @@
+# RELEASE HISTORY
+
+## 0.1.0 / 2013-01-05
+
+Honestly, I did not want to create this project, but sometimes the gods won't
+hear our prayers. So we just have to take matters into our own hands.
+
+Changes:
+
+* Autoload is alive!
+
