3efb1ab361884ccb19456ae74a3bac8a2add5839
7rans <transfire@gmail.com>
2013-01-06 09:40:58 -0500

    Fix namespace autoloading.

--- lib/autoload.rb
+++ lib/autoload.rb
@@ -1,18 +1,60 @@
 $AUTOLOAD = Hash.new{ |h, k| h[k] = [] }
 
 #
+# Toplevel autoload.
 #
-#
-def self.autoload(name, path)
-  $AUTOLOAD[name.to_sym] << path
+def self.autoload(cname, path)
+  $AUTOLOAD[[Object, cname.to_sym]] << path
+end
+
+class Object
+  #
+  #
+  #
+  def self.const_missing(name)
+    if paths = $AUTOLOAD.delete([self, name])
+      paths.each do |path|
+        require(path)
+      end
+      super(name) unless const_defined?(name)
+      const_get(name)
+    else
+      super(name)
+    end
+  end
+
 end
 
 class Module
   #
+  # Module autoload.
+  #
+  def autoload(cname, path)
+    $AUTOLOAD[[self, cname.to_sym]] << path
+  end
+
   #
   #
-  def self.autoload(subname, path)
-    $AUTOLOAD["#{name}::#{subname}".to_sym] << path
+  #
+  def const_missing(name)
+    if paths = $AUTOLOAD.delete([self, name])
+      paths.each do |path|
+        require(path)
+      end
+      super(name) unless const_defined?(name)
+      const_get(name)
+    else
+      super(name)
+    end
+  end
+end
+
+class Class
+  #
+  # We define autoload on Class too, just to be sure.
+  #
+  def autoload(cname, path)
+    $AUTOLOAD[[self, cname.to_sym]] << path
   end
 end
 
@@ -22,25 +64,8 @@ module Kernel
   # The end used can just as easily and more cleary use `self.class.autoload`
   # to do it themselves.
   #
-  def autoload(subname, path)
-    $AUTOLOAD["#{self.class.name}::#{subname}".to_sym] << path
-  end
-end
-
-class Object
-  #
-  #
-  #
-  def self.const_missing(name)
-    if paths = $AUTOLOAD.delete(name)
-      paths.each do |path|
-        require(path)
-      end
-      super(name) unless const_defined?(name)
-      const_get(name)
-    else
-      super(name)
-    end
+  def autoload(cname, path)
+    $AUTOLOAD[[self.class, cname.to_sym]] << path
   end
 end
 
