37ff0c59965e5c1422903ee61c1a7043b14cdfe5
7rans <transfire@gmail.com>
2013-01-08 08:31:45 -0500

    Fix super call bug, and simplify code.

--- demo/02_module.md
+++ demo/02_module.md
@@ -1,10 +1,15 @@
 ## Module#autoload
 
-An autoload can be used within a module for constants relative to its namespace. 
+Autoload can be used within a module for constants relative to its namespace. 
 
     module ::And
       autoload :Me, 'andme'
     end
 
     ::And::Me  #=> true
+
+Note we have to the toplevel indicators here b/c `require 'andme'` will load
+the `And` module into the toplevel, but QED executes demos in a special 
+module's scope (which emulates the toplevel, but as can be seen from this
+demo, it is not possible to do so perfectly.)
 
--- lib/autoload.rb
+++ lib/autoload.rb
@@ -1,71 +1,80 @@
+# Global table to track constants that need to be autoloaded if accessed.
 $AUTOLOAD = Hash.new{ |h, k| h[k] = [] }
 
 #
-# Toplevel autoload.
+# Toplevel autoload method.
+#
+# @param [#to_sym] cname 
+#   The constants name.
+#
+# @param [String] path
+#   File path to require.
+#
+# @return [String] The $AUTOLOAD table.
 #
 def self.autoload(cname, path)
   $AUTOLOAD[[Object, cname.to_sym]] << path
 end
 
-class Object
+module Kernel
   #
+  # Instance level autoload method.
   #
+  # Note: I am not so certain the instance level method is a good idea.
+  # The end used can just as easily and more cleary use `self.class.autoload`
+  # to do it themselves. Nonetheless, Ruby supported this so we will too.
   #
-  def self.const_missing(name)
-    if paths = $AUTOLOAD.delete([self, name])
-      paths.each do |path|
-        require(path)
-      end
-      super(name) unless const_defined?(name)
-      const_get(name)
-    else
-      super(name)
-    end
+  # @param [#to_sym] cname 
+  #   The constants name.
+  #
+  # @param [String] path
+  #   File path to require.
+  #
+  # @return [String] The $AUTOLOAD table.
+  #
+  def autoload(cname, path)
+    $AUTOLOAD[[self.class, cname.to_sym]] << path
   end
-
 end
 
 class Module
   #
-  # Module autoload.
+  # Module/Class level autoload method.
+  #
+  # @param [#to_sym] cname 
+  #   The constants name.
+  #
+  # @param [String] path
+  #   File path to require.
+  #
+  # @return [String] The $AUTOLOAD table.
   #
   def autoload(cname, path)
     $AUTOLOAD[[self, cname.to_sym]] << path
   end
 
+ private
+
+  alias :const_missing_without_autoload :const_missing
+
   #
+  # Check the $AUTOLOAD table for a `[self, name]` entry. If present,
+  # require file and try to get the constant again.
   #
+  # @param [#to_sym] cname 
+  #   The constants name.
   #
   def const_missing(name)
     if paths = $AUTOLOAD.delete([self, name])
       paths.each do |path|
         require(path)
       end
-      super(name) unless const_defined?(name)
+      const_missing_without_autoload(name) unless const_defined?(name)
       const_get(name)
     else
-      super(name)
+      const_missing_without_autoload(name)
     end
   end
-end
 
-class Class
-  #
-  # We define autoload on Class too, just to be sure.
-  #
-  def autoload(cname, path)
-    $AUTOLOAD[[self, cname.to_sym]] << path
-  end
-end
-
-module Kernel
-  #
-  # Note: I am not so certain the instance level method is a good idea.
-  # The end used can just as easily and more cleary use `self.class.autoload`
-  # to do it themselves.
-  #
-  def autoload(cname, path)
-    $AUTOLOAD[[self.class, cname.to_sym]] << path
-  end
 end
 
